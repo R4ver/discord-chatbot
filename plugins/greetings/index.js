da73e64c14954ac257788ee8a75fcc35dde32661
RavingAPD <raver13372@gmail.com>
2016-04-25 17:06:19 +0200

    Making greetings code smarter

--- plugins/greetings/index.js
+++ plugins/greetings/index.js
@@ -4,28 +4,60 @@ const runtime = require('../../utils/Runtime');
 const auth = require('../op/auth');
 const regex = new RegExp( /^(hello|hi|sup|hello guys|whats up|what's up|hi guys)$/ );
 
-const potatoGreetings = [
-    "Sup fagget",
-    "What's up potato",
-    "Please leave.",
-    "Hey look, it's the potato!"
-];
+//Different greetings based on rank
+
+const greetings = {
+    "admin": [
+        "Welcome my lord"
+    ],
+
+    "mod": [
+        "Welcome friend."
+    ],
+
+    "vip": [
+        "Welcome fam."
+    ],
+
+    "donator": [
+        "Welcome xaxaxaxa."
+    ],
+
+    "potato": [
+        "Sup fagget",
+        "What's up potato",
+        "Please leave.",
+        "Hey look, it's the potato!"
+    ]
+};
 
 module.exports = [{
-    name: 'rules',
+    name: 'greetings',
     types: ['message'],
     regex: regex,
     action: function( chat, stanza ) {
+        console.log("USING THE LOCAL BOT");
+
         //get the info about the users
         let rawEvent = stanza.rawEvent;
 
         let userID = rawEvent.userID;
-        let randomNumber = Math.floor(Math.random() * potatoGreetings.length);
-
         console.log(userID);
 
-        if ( auth.has(userID, "potato") ) {
-            chat.sendMessage(potatoGreetings[randomNumber], stanza);
+        for ( let ranks in greetings ) {
+            let keys = Object.keys(greetings);
+
+            if ( auth.has(userID, ranks) ) {
+                let randomNumber = Math.floor(Math.random() * greetings[ranks].length);
+
+                chat.sendMessage(greetings[ranks][randomNumber], stanza);
+                return;
+            }
         }
+
+        //Check if user is potato
+        // if ( auth.has(userID, "potato") ) {
+        //     chat.sendMessage(potatoGreetings[randomNumber], stanza);
+        // }
     }
 }];
