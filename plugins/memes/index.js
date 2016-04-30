68cbaafdd21993971318e9ea07fef850d04dcf66
RavingAPD <raver13372@gmail.com>
2016-04-30 04:16:07 +0200

    Added Keliras meme. Made command to add new memes to a database

--- plugins/memes/index.js
+++ plugins/memes/index.js
@@ -8,10 +8,28 @@ const h3h3Regex = new RegExp( /^(h3h3|ethan|bradberry)$/ );
 const brainPowerRegex = new RegExp( /^(brain|power|brain power|~power)$/ );
 const wakeUpRegex = new RegExp( /^(wake me up|wake me up inside|save me)$/ );
 const awpRegex = new RegExp( /^(~)awp$/ );
+const kelsRegex = new RegExp ( /^(~)lonedig$/ );
 const addMemeRegex = new RegExp( /^(~)meme\.add\smeme=(.+)\sregex=(.+)$/ );
+const removeMemeRegex = new RegExp( /^(~)meme\.remove\s(.+)$/ );
 const runMemeRegex = new RegExp ( /(.+)$/ );
 
 module.exports = [{
+    name: 'Lone digger',
+    types: ['message'],
+    regex: kelsRegex,
+    action: function( chat, stanza ) {
+        chat.sendMessage(`
+Baby can you move it round the rhythm
+So we can get with 'em
+To the ground and get us a rock and roll round
+Just a downtown body body coming with a super-hottie
+Let's go, yes, no, hell no
+Baby can you move it round the rhythm
+Cause you know we're living in the fast lane, speed up
+It ain't no game, just turn up all the beams when I come up on the scene
+`, stanza);
+    }
+},{
     name: 'I want to go in my sleep',
     types: ['message'],
     regex: goinsleepRegex,
@@ -64,7 +82,6 @@ module.exports = [{
         
         let message = stanza.message.toLowerCase();
         let match = wakeUpRegex.exec( message );
-        console.log(match[0]);
 
         if ( match[0] == "wake me up" ) {
             chat.sendMessage(`WAKE ME UP INSIDE`, stanza);
@@ -113,8 +130,6 @@ module.exports = [{
         let memeDB = runtime.brain.get('memeDB') || {};
         let newMemeID = Object.keys(memeDB).length + 1;
 
-        console.log(regex)
-
         //First check if Regex matches any existing regex
         for ( let dbRegex in memeDB ) {
             let key = memeDB[dbRegex];
@@ -125,10 +140,7 @@ module.exports = [{
             }
         }
 
-        console.log("The regex from outside: ", regex);
-
         if ( memeDB[newMemeID] === undefined ) {
-            console.log("The regex is: ", regex);
 
             memeDB[newMemeID] = {
                 id: newMemeID,
@@ -150,6 +162,43 @@ module.exports = [{
         let message = stanza.message.toLowerCase();
         let match = runMemeRegex.exec( message );
 
+        //Get the meme database
+        let memeDB = runtime.brain.get('memeDB');
+
+        //Meme from regex
+        let meme = match[1];
+
+        for ( let memes in memeDB ) {
+            let dbMeme = memeDB[memes];
+
+            if ( meme == dbMeme.regex) {
+                chat.sendMessage(dbMeme.meme, stanza);
+            }
+        }
+    }
+},{
+    name: 'Remove meme',
+    types: ['message'],
+    regex: removeMemeRegex,
+    action: function( chat, stanza ) {
+        //Get the regex content
+        let message = stanza.message.toLowerCase();
+        let match = removeMemeRegex.exec( message );
+
+        //Get the meme database
+        let memeDB = runtime.brain.get('memeDB');
+
+        //Meme from regex
         let meme = match[2];
+
+        for ( let memes in memeDB ) {
+            let dbMeme = memeDB[memes];
+
+            if ( meme == dbMeme.regex) {
+                delete memeDB[dbMeme.id];
+                runtime.brain.set("memeDB", memeDB);
+                chat.sendMessage(`Removed meme: ${meme}`, stanza);
+            }
+        }
     }
 }];
