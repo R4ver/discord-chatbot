cb2e7d742f98e38fcabd1980879c225d556c2c5b
RavingAPD <raver13372@gmail.com>
2016-05-02 21:37:25 +0200

    Added more insults

--- commands/insult.js
+++ commands/insult.js
@@ -6,7 +6,24 @@ const regex = new RegExp( /^(~)insult\s\<@((\d)+)>$/ );
 let insults = [
     "Suck my chubby.",
     "Kys (:",
-    "Fite me."
+    "Fite me.",
+    "Anyone who ever loved you was wrong.",
+    "If you were anymore inbred you would be a sandwich.",
+    "Now I know why everybody talks about you behind your back.",
+    "your gene pool could use a little chlorine.",
+    "The best part of you ran down your mother’s legs.",
+    "You coffin dodging oxygen thief.",
+    "What doesn’t kill you…disappoints me.",
+    "If laughter is the best medicine, your face must be curing the world.",
+    "The only way you’ll ever get laid is if you crawl up a chicken’s ass and wait.",
+    "It looks like your face caught fire and someone tried to put it out with a hammer.",
+    "I’d like to see things from your point of view, but I can’t seem to get my head that far up your ass.",
+    "You’ll never be the man your mother is.",
+    "Save your breath – you’ll need it to blow up your date.",
+    "You are proof that evolution can go in reverse.",
+    "I thought of you today. It reminded me to take the garbage out.",
+    "I’d slap you but I don’t want to make your face look any better.",
+    "You have the right to remain silent because whatever you say will probably be stupid anyway."
 ]
 
 module.exports = [{
